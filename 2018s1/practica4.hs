c52e39c9dd8fcdbe014f58dbeba89cb304a1d952
nicolas <nicolastpi10@gmail.com>
2018-05-14 01:01:03 -0300

    demostraciones inductivas -- Terminado

--- 2018s1/practica4.hs
+++ 2018s1/practica4.hs
@@ -77,7 +77,7 @@ mapMTree f (Lm maybe) = Lm (mapMaybe f maybe)
 mapMTree f (Bm x mt1 mt2) = Bm (f x) (mapMTree f mt1) (mapMTree f mt2)
 
 
---10) (Desafío)
+--10) (Desafío) --> (pendiente por ahora)
 
 data GenTree a = GNode a [GenTree a] deriving (Show)
 
@@ -89,6 +89,8 @@ data GenTree a = GNode a [GenTree a] deriving (Show)
 -- Con la definición de map dada para el tipo T del ejercicio anterior (llamémosla mapX), demostrar:
 
 --1)
+
+ {- Todo dentro de este comentario largo son las demostraciones inductivas :s
  
 mapX id = id
                     = ppio extensionalidad
@@ -150,18 +152,74 @@ Demostrado caso 2 .
 
 -----------------------------------------------------------------------------------------
 
---data T a = A a | B (T a) | C (T a) (T a) deriving (Show)
+2. mapX f . mapX g = mapX (f.g) (Llamada propiedad de fusión)
 
---mapT :: (a -> b) -> T a -> T b
---mapT f (A x) = A (f x)
---mapT f (B t) = B (mapT f t)
---mapT f (C t1 t2) = C (mapT f t1) (mapT f t2)
+mapX f . mapX g = mapX (f.g)
+                                      = ppio extensionalidad
+(mapX f . mapX g) t = mapX (f.g) t
+                                      = def (.)
+mapX f (mapX g t) = mapX (f.g) t
 
-t = NodeT x t1 t2
+Demuestro por inducción estructural en t
 
-HI) t1 se verifica
-HI) t2 se verifica
+Caso base, t = (A x)
 
+mapX f (mapX g (A x))
+                             = def mapX
+mapX f (A (g x))
+                             = def mapX
+A (f (g x))
+
+mapX (f.g) (A x)
+                     = def mapX
+A ((f.g) x)
+                     = def (.)
+A (f (g x))
+
+-- 2 Casos inductivos:
+
+Caso 1, t = (B r)    -> (r = recursive)
+
+
+HI) mapX f (mapX g r) = mapX (f.g) r
+TI) mapX f (mapX g (B r)) = mapX (f.g) (B r)
+
+
+mapX f (mapX g (B r))
+                         = def mapX
+mapX f (B (mapX g r))
+                         = def mapX
+B (mapX f (mapX g r))
+                         = HI
+B (mapX (f.g) r)
+                         = def mapX (al revés)
+mapX (f.g) (B r)
+
+Demostrado caso 1 .
+
+Caso 2, t = (C r1 r2) 
+
+HI) 
+1) mapX f (mapX g r1) = mapX (f.g) r1
+2) mapX f (mapX g r2) = mapX (f.g) r2
+TI) mapX f (mapX g (C r1 r2)) = mapX (f.g) (C r1 r2)
+
+
+mapX f (mapX g (C r1 r2))
+                                            = def mapX
+mapX f (C (mapX g r1) (mapX g r2))
+                                            = def mapX
+C (mapX f (mapX g r1)) (mapX f (mapX g r2))
+                                            = HI 1) y 2)
+C (mapX (f.g) r1) (mapX (f.g) r2)
+                                            = def mapX (al revés)
+mapX (f.g) (C r1 r2)
+
+                       QED
+
+-------------------------------------------------------------------------------------------
+
+-}
 
 
 
